b320f0cdb426f89dc14a53270c64a585b98edc89
thaisperlho <carvalho.thais0301@gmail.com>
2023-11-03 14:05:54 -0300

    creating header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 79px;
+    text-align: left;
+    background: #F4D3DD;
+    `;
+
+    export const Header = styled.h1``;
+
+    export const Title = styled.div`
+        color: #ED217C;
+        padding-top: 20px;
+        padding-left: 10px;
+        `;
